988a42d4786dcbdc7c4a66a87004efdf5fd47b8a
ritu <ritugondaliya11@gmail.com>
2023-02-17 18:10:27 +0530

    mission listing, list view, grid view

---
+++ MVC/CI Platform/test/wwwroot/js/coustom.js
@@ -0,0 +1,21 @@
+﻿var navbar = document.getElementById("nav2-menu-button");
+console.log(navbar);
+
+
+navbar.addEventListener("click", function () {
+    console.log("clicked");
+});
+//function openFilter() {
+
+
+//    document.getElementById("header2sm").style.display = "none";
+//    //document.getElementById("navbarSupportedContent").style.display = "none";
+//    //document.getElementById("searchBar").style.display = "none";
+//    //document.getElementById("FilterSupportedContent").style.marginLeft = "250px";
+//}
+
+//function closeFilter() {
+//    document.getElementById("header2sm").style.display = "block";
+//    //  document.getElementById("searchBar").style.display = "block";
+//    //  document.getElementById("FilterSupportedContent").style.marginLeft = "250px";
+//    //}
