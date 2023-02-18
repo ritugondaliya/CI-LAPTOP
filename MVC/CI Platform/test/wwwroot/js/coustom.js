7fcd5f808c7f3ecafc28f8cce2060dcd98c8e406
ritu <ritugondaliya11@gmail.com>
2023-02-18 15:53:36 +0530

    mission listing, list view, grid view

--- MVC/CI Platform/test/wwwroot/js/coustom.js
+++ MVC/CI Platform/test/wwwroot/js/coustom.js
@@ -1,11 +1,11 @@
-﻿var navbar = document.getElementById("nav2-menu-button");
-console.log(navbar);
+﻿//var navbar = document.getElementById("nav2-menu-button");
+//console.log(navbar);
 
 
-navbar.addEventListener("click", function () {
-    console.log("clicked");
-});
-//function openFilter() {
+//navbar.addEventListener("click", function () {
+//    console.log("clicked");
+//});
+////function openFilter() {
 
 
 //    document.getElementById("header2sm").style.display = "none";
@@ -19,3 +19,18 @@ navbar.addEventListener("click", function () {
 //    //  document.getElementById("searchBar").style.display = "block";
 //    //  document.getElementById("FilterSupportedContent").style.marginLeft = "250px";
 //    //}
+
+
+function showList(e) {
+    var $gridCont = $('.grid-container');
+    e.preventDefault();
+    $gridCont.hasClass('list-view') ? $gridCont.removeClass('list-view') : $gridCont.addClass('list-view');
+}
+function gridList(e) {
+    var $gridCont = $('.grid-container')
+    e.preventDefault();
+    $gridCont.removeClass('list-view');
+}
+
+$(document).on('click', '.btn-grid', gridList);
+$(document).on('click', '.btn-list', showList);
