d3d9efdecad79270e906a5ff52e410438529b4da
ritu <ritugondaliya11@gmail.com>
2023-02-20 18:22:12 +0530

    NoMissionFound

--- MVC/CI Platform/test/Controllers/HomeController.cs
+++ MVC/CI Platform/test/Controllers/HomeController.cs
@@ -42,6 +42,10 @@ namespace test.Controllers
         {
             return View();
         }
+        public IActionResult NoMissionFound()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ MVC/CI Platform/test/wwwroot/js/custom.js
@@ -0,0 +1,105 @@
+﻿
+
+
+
+function showList(e) {
+    var $gridCont = $('.grid-container');
+    e.preventDefault();
+    $gridCont.hasClass('list-view') ? $gridCont.removeClass('list-view') : $gridCont.addClass('list-view');
+}
+function gridList(e) {
+    var $gridCont = $('.grid-container')
+    e.preventDefault();
+    $gridCont.removeClass('list-view');
+}
+
+$(document).on('click', '.btn-grid', gridList);
+$(document).on('click', '.btn-list', showList);
+
+
+
+// check box click element
+
+
+var checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+/*let filtersSection_ = document.querySelector(".filters-section11");*/
+
+
+//var listArray = [];
+
+var filterList = document.querySelector(".filter-list");
+
+//var len = listArray.length;
+
+
+for (var checkbox of checkboxes) {
+    console.log(checkbox);
+    checkbox.addEventListener("click", function () {
+        if (this.checked == true) {
+            console.log("check");
+            addElement(this, this.value);
+        }
+        else {
+
+            removeElement(this.value);
+            console.log("unchecked");
+        }
+    })
+}
+
+
+function addElement(current, value) {
+    let filtersSection = document.querySelector(".filters-section11");
+    console.log("inside add");
+    let createdTag = document.createElement('span');
+    createdTag.classList.add('filter-list');
+    createdTag.classList.add('ps-3');
+    createdTag.classList.add('pe-1');
+    createdTag.classList.add('me-2');
+    createdTag.innerHTML = value;
+
+    createdTag.setAttribute('id', value);
+    let crossButton = document.createElement('button');
+    crossButton.classList.add("filter-close-button");
+    let cross = '&times;'
+
+    crossButton.addEventListener('click', function () {
+        let elementToBeRemoved = document.getElementById(value);
+
+        console.log(elementToBeRemoved);
+        console.log(current);
+        elementToBeRemoved.remove();
+
+        current.checked = false;
+    })
+
+    crossButton.innerHTML = cross;
+
+    // let crossButton = '&times;'
+
+    createdTag.appendChild(crossButton);
+    filtersSection.appendChild(createdTag);
+
+}
+
+function removeElement(value) {
+
+    let filtersSection = document.querySelector(".filters-section");
+
+    let elementToBeRemoved = document.getElementById(value);
+    filtersSection.removeChild(elementToBeRemoved);
+
+}
+
+
+// for clear all
+
+clearbutton.onclick = () => {
+    const myNode = document.querySelector(".filters-section11");
+    while (myNode.lastElementChild) {
+        myNode.removeChild(myNode.lastElementChild);
+    }
+
+
+}
